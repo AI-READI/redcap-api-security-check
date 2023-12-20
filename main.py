3b0ade00887f41a27c2ded7d235287434cd1085c
Erik Benton <benton@ohsu.edu>
2023-12-19 22:21:26 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+import pandas
+import sys
+
+from dotenv import load_dotenv
+from redcap import Project
+
+load_dotenv()
+token = os.getenv('REDCAP_TOKEN')
+url = os.getenv('REDCAP_URL')
+
+prj = Project(url, token)
+df = prj.export_metadata(format_type='df')
+
+phi_fields = df[df["identifier"] == "y"]
+
+data_df = prj.export_records(fields = phi_fields.index.to_list(), format_type = "df")
+
+if len(data_df.columns.to_list()) > 2:
+    sys.exit(1)
