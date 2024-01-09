1a77a8779686f8dbbfe810d924cf13aeea638cfa
Erik Benton <benton@ohsu.edu>
2024-01-09 08:03:41 -0800

    Improvements to reading environment

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python
 
-import os
-import sys
-
-from dotenv import load_dotenv
+from dotenv import dotenv_values
+from os import environ
+from pathlib import Path
 from redcap import Project
+from sys import exit
 
-load_dotenv()
-token = os.getenv('REDCAP_TOKEN')
-url = os.getenv('REDCAP_URL')
+# Check if `.env` file exists
+env_path = Path(".") / ".env"
+
+LOCAL_ENV_FILE = env_path.exists()
+
+config = dotenv_values(".env")
+
+def get_env(key):
+    """Return environment variable from .env or native environment."""
+    if LOCAL_ENV_FILE:
+        return config.get(key)
+
+    if key not in environ:
+        raise ValueError(f"Environment variable {key} not set.")
+
+    return environ.get(key)
+
+token = get_env('REDCAP_TOKEN')
+url = get_env('REDCAP_URL')
 
 prj = Project(url, token)
 df = prj.export_metadata(format_type='df')
@@ -17,5 +33,4 @@ phi_fields = df[df["identifier"] == "y"]
 
 data_df = prj.export_records(fields = phi_fields.index.to_list(), format_type = "df")
 
-if len(data_df.columns.to_list()) > 2:
-    sys.exit(1)
+assert len(data_df.columns.to_list()) == 2, "REDCap returned invalid number of columns"
